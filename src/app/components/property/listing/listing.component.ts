30ee7731f35b35304f7d27062c841f8c774e5de0
Mike Ekert <mike.ekert@totalexpert.com>
2018-06-17 21:16:42 -0500

    bug fixes + listings made into models/repeatable

--- src/app/components/property/listing/listing.component.ts
+++ src/app/components/property/listing/listing.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Listing} from '../../../models/listing';
 
 @Component({
   selector: 'app-listing',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListingComponent implements OnInit {
 
-  constructor() { }
+  @Input() Listing: Listing;
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
